9c731f5bb36dd803a1214659689d7d84fa90da85
Ankhaa0422 <ankhaa.akire@gmail.com>
2024-05-23 20:46:17 +0800

    zs

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -6,7 +6,7 @@ export async function POST(req) {
   try {
     const body = await req.json();
     const result = await axios
-      .post("https://api.bbroi.com/auth/login", body)
+      .post("https://api.bbroi.com/master/login", body)
       .then((res) => res.data);
 
     if (result.status === 200) {
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,16 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { useMainCtx } from "@/context/main";
-
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
+import axios from "axios";
 export default function Login() {
 
   const { mainData, onChangeMainData } = useMainCtx()
-
+  const router = useRouter()
   const [data, setData] = useState({})
 
   function set(obj = {}) {
@@ -20,46 +23,81 @@ export default function Login() {
     set({[e.target.name]: e.target.value})
   }
 
-  return (
-    <div className="h-screen flex items-center justify-center bg-[#101010] text-white">
-      <div
-        style={{
-          backgroundImage: `url("login_bg.jpg")`,
-        }}
-        className="w-[746px] h-[915px] flex flex-col items-center"
-      >
-        <div className="w-full flex justify-center">
-          <Image src="/bbroi_logo.png" alt="logo" width={318} height={175} />
-        </div>
-        <div className="text-4xl mb-[65px]">ADMIN PANEL</div>
-        <div className="w-[344px] flex flex-col items-center">
-          <div className="relative h-[45px] w-full mb-[30px]">
-            <img
-              alt="username"
-              src="/icons/user.png"
-              className="h-4 w-4 absolute top-0 bottom-0 my-auto left-3"
-            />
-            <Input
-              placeholder="USERNAME"
-              className="w-full h-[45px] bg-[#09080E] text-white placeholder:text-white pl-9 border-none"
-            />
+  async function onLogin() {
+    try {
+      const body = {
+        username: data.username,
+        password: data.password,
+      };
+      const result = await axios.post("/api/login", body).then((res) => res.data);
+
+      if (result.status === 200) {
+        const session = await signIn("credentials", { redirect: false });
+        if (session.status === 200) {
+          toast.success(result.message);
+          mainData.isAuth = true;
+          onChangeMainData();
+          onClose();
+        }
+      } else toast.error(result.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+  if(!mainData.isAuth) {
+    return (
+      <div className="h-screen flex items-center justify-center bg-[#101010] text-white">
+        <div
+          style={{
+            backgroundImage: `url("login_bg.jpg")`,
+          }}
+          className="w-[746px] h-[915px] flex flex-col items-center"
+        >
+          <div className="w-full flex justify-center">
+            <Image src="/bbroi_logo.png" alt="logo" width={318} height={175} />
           </div>
-          <div className="relative h-[45px] w-full mb-[40px]">
-            <img
-              alt="password"
-              src="/icons/password.png"
-              className="h-4 w-4 absolute top-0 bottom-0 my-auto left-3"
-            />
-            <Input
-              placeholder="PASSWORD"
-              className="w-full h-[45px] bg-[#09080E] text-white placeholder:text-white pl-9 border-none"
-            />
+          <div className="text-4xl mb-[65px]">ADMIN PANEL</div>
+          <div className="w-[344px] flex flex-col items-center">
+            <div className="relative h-[45px] w-full mb-[30px]">
+              <img
+                alt="username"
+                src="/icons/user.png"
+                className="h-4 w-4 absolute top-0 bottom-0 my-auto left-3"
+              />
+              <Input
+                id="username"
+                name="username"
+                type="username"
+                placeholder="USERNAME"
+                className="w-full h-[45px] bg-[#09080E] text-white placeholder:text-white pl-9 border-none"
+                value={data?.username}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="relative h-[45px] w-full mb-[40px]">
+              <img
+                alt="password"
+                src="/icons/password.png"
+                className="h-4 w-4 absolute top-0 bottom-0 my-auto left-3"
+              />
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="PASSWORD"
+                className="w-full h-[45px] bg-[#09080E] text-white placeholder:text-white pl-9 border-none"
+                value={data?.password}
+                onChange={handleChange}
+              />
+            </div>
+            <Button className="w-[163px] h-[45px] bg-[#09080E] rounded-[5px]" onClick={() => { onLogin() }}>
+              LOGIN
+            </Button>
           </div>
-          <Button className="w-[163px] h-[45px] bg-[#09080E] rounded-[5px]">
-            LOGIN
-          </Button>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return router.push('/profit')
+  }
 }
